2e065cfd7d8d250610eb8de5a1b236404f3e3bac
MedSveta <sveta1978medved@gmail.com>
2026-01-14 21:01:01 +0200

    Api Registration Test

---
+++ src/test/java/rest_tests/RegistrationTestsRest.java
@@ -0,0 +1,51 @@
+package rest_tests;
+
+import api_rest.AuthenticationController;
+import dto.ErrorMessageDtoString;
+import dto.RegistrationBodyDto;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.util.Random;
+
+public class RegistrationTestsRest extends AuthenticationController {
+    SoftAssert softAssert = new SoftAssert();
+
+    @Test
+    public void registrationPositiveTest() {
+        int i = new Random().nextInt(1000);
+        RegistrationBodyDto user = RegistrationBodyDto.builder()
+                .username("polly_polly" + i + "@gmail.com")
+                .password("Password123!")
+                .firstName("Polly")
+                .lastName("Polly")
+                .build();
+        Assert.assertEquals(registrationLogin(user,
+                REGISTRATION_URL).getStatusCode(), 200);
+    }
+
+    @Test
+    public void registrationNegativeTest_WrongEmail() {
+        int i = new Random().nextInt(1000);
+        RegistrationBodyDto user = RegistrationBodyDto.builder()
+                .username("polly_polly" + i + "gmail.com")
+                .password("Password123!")
+                .firstName("Polly")
+                .lastName("Polly")
+                .build();
+        Response response = registrationLogin(user, REGISTRATION_URL);
+        softAssert.assertEquals(response.getStatusCode(),
+                400, "validate status code");
+        ErrorMessageDtoString errorMessageDtoString = response
+                .getBody().as(ErrorMessageDtoString.class);
+        System.out.println(errorMessageDtoString);
+        softAssert.assertEquals(errorMessageDtoString.getError(),
+                "Bad Request", "validate error name");
+        softAssert.assertTrue(errorMessageDtoString.getMessage().toString()
+                .contains("must be a well-formed email address"),
+                "validate message");
+        softAssert.assertAll();
+    }
+}
